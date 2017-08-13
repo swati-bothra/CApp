1a7f27a6cdd41ac73ac5320b82ed34b7e6af708d
YoloHealth <auto@yolohealth.in>
2017-08-13 09:51:30 +0530

    initial commit

---
+++ app/src/main/java/com/example/yolo/capp/PatternLock.java
@@ -0,0 +1,14 @@
+package com.example.yolo.capp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PatternLock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pattern_lock);
+
+    }
+}
